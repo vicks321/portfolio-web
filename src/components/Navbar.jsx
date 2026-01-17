e96a553de893f0c0c69da22667c69c8aee101832
Vicky Sah <vickysah776@gmail.com>
2026-01-17 13:43:13 +0545

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,38 +7,40 @@ const Navbar = () => {
 
   return (
     <nav className=" border-b md:w-[40%] mx-auto rounded-4xl px-6 py-4 flex justify-between items-center">
-      {/* Logo / Title */}
-      <div className=" text-2xl font-bold text-white hover:cursor">
+      
+      <div className=" text-base font-bold text-white hover:cursor">
        <a href="/">Vicky</a>
         </div>
 
-      {/* Desktop Links */}
-      <div className="hidden text-3xl md:flex gap-15 items-center text-white ">
+    
+      <div className="hidden text-shadow-initial md:flex gap-10 items-center text-white ">
         
         
-        <a href="/home" className="hover:opacity-100">Home</a>
+        <a href="/" className="hover:opacity-100">Home</a>
         <a href="/about" className="hover:opacity-100">About</a>
         <a href="/projects" className="hover:opacity-100">Projects</a>
         <a href="https://vicks.hashnode.dev" target="_blank" className="hover:opacity-100">Blog</a>
       </div>
 
-      {/* Desktop Icons */}
+     
       <div className="hidden md:flex gap-4 items-center">
         <a href="https://github.com/vicks321" target="_blank" className=" hover:opacity-100 text-2xl text-white"><FaGithubSquare /></a>
         <a href="https://www.linkedin.com/in/vicky-sah-707954350" target="_blank" className=" hover:opacity-100 text-4xl text-white"><FaLinkedin /></a>
         <a href="https://www.instagram.com/_vicks_321" target="_blank" className=" hover:opacity-100 text-2xl text-white"><GrInstagram /></a>
       </div>
 
-      {/* Mobile Menu Button */}
-      <div className="md:hidden text-white text-2xl" onClick={() => setIsOpen(!isOpen)}>
+    
+      <div className="text-white text-2xl md:hidden w-8 h-8 flex items-center justify-center" onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? <FaTimes /> : <FaBars />}
       </div>
 
-      {/* Mobile Dropdown */}
+      
       {isOpen && (
-        <div className=" top-20 left-0 w-full flex flex-col items-center gap-4 py-4 md:hidden">
+        <div className=" top-20 left-0 w-full flex flex-col items-center gap-4 py-4">
           
           
+          
+          <a href="/" className="text-white text-lg">Home</a>
           <a href="/about" className="text-white text-lg">About</a>
           <a href="/projects" className="text-white text-lg">Projects</a>
           <a href="https://vicks.hashnode.dev" className="text-white text-lg">Blog</a>
