50d5ec3f9d3ea55cb05727b491e8a116a22add63
Vicky Sah <vickysah776@gmail.com>
2026-01-19 14:49:28 +0545

    some fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,14 @@ import AgeCounter from './Dob'
 
 const Home = () => {
   return (
-    <div className='md:w-[40%] flex mx-auto mt-6 '>
+    <div className='flex-col md:w-[40%] flex mx-auto mt-6 '>
       <div>
         <h1 className='text-4xl  font-bold'>Hi, I`m <span className='font-extoldrab text-amber-900'>Vicky</span> - an engineering Student</h1>
         
         <p className='mt-3'>I’m Vicky — a Computer Engineering student passionate about coding, web development, and building things that actually work. I’m always learning, always improving, and excited to create something meaningful.🚀</p>
         <AgeCounter/>
       </div>
-     
-
+    
     
     </div>
   )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='md:w-[40%] flex mx-auto justify-center text-center text-4xl items-center'>
-      <a className='font-bold text-6xl text-zinc-200 mt-28'>Under Construction</a>
+      <a className='font-bold text-5xl text-zinc-200 mt-28'>Under Construction</a>
     </div>
   )
 }
